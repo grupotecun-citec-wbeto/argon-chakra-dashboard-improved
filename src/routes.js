fc8d8dc17d50e80c5937b0849a361363ff6631c9
Humberto Antonio Herrador Reyes <47198640+wbeto@users.noreply.github.com>
2024-09-25 13:29:28 -0600

    Se agrego propiedad para eliminar un item del slidebar, pero la ruta si exista

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ import SignIn from "views/Pages/SignIn.js";
 import SignUp from "views/Pages/SignUp.js";
 
 // - NOTE
-// - In routes active redirect, is not visible en slidebar, without content only ""
+// - In routes active redirect:"#", is not visible en slidebar, without content only "#"
 
 import {
   HomeIcon,
@@ -116,6 +116,43 @@ var dashRoutes = [
         secondaryNavbar: true,
         component: Profile,
         layout: "/admin",
+      },
+      {
+        path: "/signin",
+        name: "Sign In",
+        rtlName: "لوحة القيادة",
+        icon: <DocumentIcon color='inherit' />,
+        component: SignIn,
+        layout: "/auth",
+      },
+      {
+        path: "/signup",
+        name: "Sign Up",
+        rtlName: "لوحة القيادة",
+        icon: <RocketIcon color='inherit' />,
+        component: SignUp,
+        layout: "/auth",
+      },
+    ],
+  },
+  {
+    name: "PAGES",
+    path: "/pages",
+    category: "pages",
+    rtlName: "صفحات",
+    state: "pageCollapse",
+    layout: "/admin",
+    icon: <PersonIcon color='inherit' />,
+    views: [
+      {
+        path: "/pages/profile3",
+        name: "Profile3",
+        rtlName: "لوحة القيادة",
+        icon: <PersonIcon color='inherit' />,
+        secondaryNavbar: true,
+        component: Profile,
+        layout: "/admin",
+        redirect:"#" // 
       },
       {
         path: "/signin",
